63a5023dffb5bde8fdd24df517f60c8d06435ee1
Joe-TheBro <hammj00@gmail.com>
2018-08-21 19:40:34 -0500

    Intial Commit

---
+++ iventory/app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'me',
+  password : 'secret',
+  database : 'my_db'
+});
+
+connection.connect();
+
+app.get('/accountinfo', function (req, res) {
+  connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+    if (error) throw error;
+    console.log('The solution is: ', results[0].solution);
+  });
+})
+
+app.listen(3000)
